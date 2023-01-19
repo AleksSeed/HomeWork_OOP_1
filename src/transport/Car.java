748c514201b35727d42af89d10467ab79daf65d3
SAlekS <sas431@mail.ru>
2023-01-19 13:48:21 +0300

    8

--- src/transport/Car.java
+++ src/transport/Car.java
@@ -1,17 +1,16 @@
 package transport;
 
-
 public class Car extends Transport{
 
     private final String body;
 
     public Car(String brand, Integer productionYear, String model, String color,
-                          String productionCountry, String body, int maxSpeed){
+               String productionCountry, String body, int maxSpeed){
         super(brand, model, productionYear, productionCountry, color, maxSpeed);
         this.body = body;
     }
 
-      public String getBody() { return body; }
+    public String getBody() { return body; }
 
 
 
